42715440cfe47c17ae47c1207a94d1186475fc6c
Steve <steve@webdrive.co.nz>
2014-03-05 13:36:17 +1300

    blah

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 echo '<h1>Hello World</h1></br>';
 
-echo "<h2>Hi, my name is slim ".$_SERVER["COMPUTERNAME"]." running on ".php_uname()."</h2>";
+//$name=$_SERVER["COMPUTERNAME"];
+$name=$_SERVER["WEBSITE_INSTANCE_ID"];
+
+echo "<h2>Hi, my name is slim ".$name." running on ".php_uname()."</h2>";
 
 echo date('Y-m-d H:i:s').'<br>';
 
